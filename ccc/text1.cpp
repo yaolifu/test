c327007e2eff356113d9a26aa62378b8d60e973a
yaolifu <1148404715@qq.com>
2021-11-29 15:49:47 +0800

    loading first time

--- ccc/text1.cpp
+++ ccc/text1.cpp
@@ -4,7 +4,6 @@ int main(int argc,char *argv[])
 	printf("%d\n",argc);
 	printf("%s\n",argv[0]);
 	printf("%s\n",argv[1]);
-	printf("%s\n",argv[2]);
-	printf("%s\n",argv[3]);
+    printf("服务外包实验");
 	return 0;
  } 
